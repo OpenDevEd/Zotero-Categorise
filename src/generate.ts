a5cd857c6f6516034dcb16a51c2dbc7a4289ede4
echaoub <abdochaoubi2000@gmail.com>
2024-03-26 14:51:45 +0000

    Refactor generate function and add helper functions

--- src/generate.ts
+++ src/generate.ts
@@ -40,21 +40,65 @@ type ResList = {
   ignoretag?: string[];
 };
 
-async function generate(commanderOptions: CommanderOptions) {
-  let zotero: Zotero;
-  const groupid = commanderOptions.group;
+function getZotero(groupid: string) {
   if (!groupid) {
-    zotero = new Zotero({ verbose: false });
-  } else {
-    zotero = new Zotero({ verbose: false, 'group-id': groupid });
+    return new Zotero({ verbose: false });
   }
+  return new Zotero({ verbose: false, 'group-id': groupid });
+}
+async function addChildren(collection: Collection[], parent: any, list: ResList, commanderOptions: CommanderOptions) {
+  for (const child of parent.children) {
+    collection.push({
+      topic: child.data.name,
+      collection: `zotero://select/groups/${list.library}/collections/${child.key}`,
+      tags: commanderOptions.addtags
+        ? [commanderOptions.tagprefix ? `${commanderOptions.tagprefix}${child.data.name}` : child.data.name]
+        : [],
+      terms: [{ term: child.data.name, description: 'main', type: 'word' }],
+    });
+    if (child.children.length > 0) {
+      await addChildren(collection, child, list, commanderOptions);
+    }
+  }
+}
+
+async function makelist(
+  collectionList: ResCollection,
+  list: ResList,
+  commanderOptions: CommanderOptions,
+  results: any
+) {
+  for (const collectionkey of results) {
+    collectionList.collections = collectionList.collections || [];
+    collectionList.collections.push({
+      topic: collectionkey.data.name,
+      collection: `zotero://select/groups/${list.library}/collections/${collectionkey.key}`,
+      tags: commanderOptions.addtags
+        ? [
+            commanderOptions.tagprefix
+              ? `${commanderOptions.tagprefix}${collectionkey.data.name}`
+              : collectionkey.data.name,
+          ]
+        : [],
+      terms: [{ term: collectionkey.data.name, description: 'main', type: 'word' }],
+    });
+
+    if (commanderOptions.recursive && collectionkey.children?.length > 0) {
+      await addChildren(collectionList.collections, collectionkey, list, commanderOptions);
+    }
+    console.log(collectionkey.data);
+  }
+}
+
+async function generate(commanderOptions: CommanderOptions) {
+  let zotero: Zotero = getZotero(commanderOptions.group);
   if (!commanderOptions.collection) {
     console.log('You must provide at least one collection');
     return;
   }
 
   const list: ResList = {};
-  list.library = groupid;
+  list.library = commanderOptions.group;
   list.source_collections = [];
 
   for (const collectionId of commanderOptions.collection) {
@@ -72,7 +116,7 @@ async function generate(commanderOptions: CommanderOptions) {
     // fetch the collection
     const result: any = await zotero.collection(options);
     // check if the collection exists
-    if (!result || !result.data) {
+    if (!result?.data) {
       console.log(`There is no collection with this key ${collectionId}`);
       continue;
     }
@@ -80,45 +124,8 @@ async function generate(commanderOptions: CommanderOptions) {
     if (list.library === undefined) list.library = result.library.id;
     // fetch the subcollections
     const results: any = await zotero.collections(options);
-    // fs.writeFileSync('collections.json', JSON.stringify(results, null, 4));
-
-    async function addChildren(collection: Collection[], parent: any) {
-      for (const child of parent.children) {
-        // console.log('child name', child.data.name);
-        collection.push({
-          topic: child.data.name,
-          collection: `zotero://select/groups/${list.library}/collections/${child.key}`,
-          tags: commanderOptions.addtags
-            ? [commanderOptions.tagprefix ? `${commanderOptions.tagprefix}${child.data.name}` : child.data.name]
-            : [],
-          terms: [{ term: child.data.name, description: 'main', type: 'word' }],
-        });
-        if (child.children.length > 0) {
-          addChildren(collection, child);
-        }
-      }
-    }
-
     // make a list of the subcollections with the collection name, the collection key and the terms
-    for (const collectionkey of results) {
-      collectionList.collections.push({
-        topic: collectionkey.data.name,
-        collection: `zotero://select/groups/${list.library}/collections/${collectionkey.key}`,
-        tags: commanderOptions.addtags
-          ? [
-              commanderOptions.tagprefix
-                ? `${commanderOptions.tagprefix}${collectionkey.data.name}`
-                : collectionkey.data.name,
-            ]
-          : [],
-        terms: [{ term: collectionkey.data.name, description: 'main', type: 'word' }],
-      });
-
-      if (commanderOptions.recursive && collectionkey.children?.length > 0) {
-        await addChildren(collectionList.collections, collectionkey);
-      }
-      console.log(collectionkey.data);
-    }
+    await makelist(collectionList, list, commanderOptions, results);
     list.source_collections.push(collectionList);
   }
   list.ignoretag = [];
